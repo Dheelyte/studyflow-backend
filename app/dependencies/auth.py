b71c5cf93ef305f18bfd7f43883e5cd8c7e7309f
Delight Olu-Olagbuji <olagbujidelight@gmail.com>
2026-01-22 19:14:42 +0100

    Use CookieService regex to work with jwt

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -17,6 +17,8 @@ async def get_auth_user(
         raise UnauthorizedError("Not authenticated")
     
     email = token_service.verify_access_token(token)
+    print("DEBUG: Token", token)
+    print("DEBUG: Email", email)
     if not email:
         raise UnauthorizedError("Invalid or expired token")
         
